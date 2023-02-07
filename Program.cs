5cdc9808068c50bad0167289ed3ce86b97ccbe74
Isaque-Gomes <116127894+Isaque-Gomes@users.noreply.github.com>
2023-02-06 22:18:59 -0300

    [M1S01] Ex 2 - Área do Triângulo
    [Variáveis]
    Crie um programa que receba os valores da base e da altura de um triângulo qualquer, e calcule a área do triângulo usando a fórmula area =(base × altura)/2 e exiba o resultado para o usuário.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Calcular a área de um triângulo
+float areaT;
+float baseT;
+float alturaT;
+float calculo;
+
+Console.WriteLine("Olá! Vamos calcular a área de um triângulo qualquer.");
+Console.WriteLine("Digite a base do triângulo em números:");
+baseT = float.Parse(Console.ReadLine());
+Console.WriteLine("Agora digite a altura do triângulo em números:");
+alturaT = float.Parse(Console.ReadLine());
+calculo = (baseT * alturaT) / 2;
+Console.WriteLine($"A área do triângulo é {calculo} ");
